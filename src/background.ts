1c8608e99d5757a65ec4bc7096e4de3ecf813a33
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2022-11-26 17:30:08 +0100

    fix: remove dead code

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,6 @@
 browser.browserAction.onClicked.addListener(async tab => {
   try {
-    // browser.runtime.reload();
-
-    // const [activeTab] = await browser.tabs.query({ active: true, currentWindow: true });
-    // if (activeTab?.id) {
-    //   await browser.tabs.reload(activeTab.id);
-    // }
-
-    const executing = browser.tabs.executeScript({
+    await browser.tabs.executeScript({
       file: 'exam-parser.js',
     });
   } catch (e: unknown) {
